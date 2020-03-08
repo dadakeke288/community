afb434a69145100fab10a25566de7809ac72c574
dadakeke288 <807241093@qq.com>
2020-03-08 14:18:06 +0800

    修复上一步遗留的一点bug，还剩MongoSocketOpenException没解决，但是网页能跑

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -24,16 +24,16 @@ public class UserService {
             User dbUser = users.get(0);
             UserExample userExample1 = new UserExample();
             User updateUser = new User();
-
+            //updateUser 中传需要更新的参数
             updateUser.setGmtModified(System.currentTimeMillis());
             updateUser.setAvatarUrl(user.getAvatarUrl());
             updateUser.setName(user.getName());
             updateUser.setToken(user.getToken());
             updateUser.setBio(user.getBio());
-            //userExample1只有id值
+            //userExample1只有id值 相当于where id =
             userExample1.createCriteria().andIdEqualTo(dbUser.getId());
 
-            userMapper.updateByExample(updateUser,userExample1);
+            userMapper.updateByExampleSelective(updateUser,userExample1);
         }
     }
 }
